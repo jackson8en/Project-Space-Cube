2531d9a4ed93ef2aef1cb6a3dea9933f5fc19566
jackson8en <jackson.8en@gmail.com>
2016-11-27 23:32:46 +0000

    Mouse Manager Intial Setup
    Creating the project and scene. A Mouse manager was created to handle
    all mouse actions. Current actions available are: Left Click to Spawn
    Cube at ray hit location.

---
+++ Project Space Cube/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public GameObject thePrefabToSpawn;
+
+	// Update is called once per frame
+	void Update () {
+		// Was the mouse pressed down this frame
+		if(Input.GetMouseButtonDown(0)){ 
+			// Left mouse button was pressed
+			// Is the mouse over an object
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hitInfo;
+
+			if (Physics.Raycast (ray, out hitInfo)) {
+				Debug.Log ("Hit: " + hitInfo.collider.gameObject.name);
+
+				// Spawn new object
+				Vector3 spawnLocation = hitInfo.point;
+				Instantiate(thePrefabToSpawn, spawnLocation, Quaternion.identity);
+			}
+		}
+	
+	}
+		
+}
