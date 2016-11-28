f65d3e2f67f5de78bffcc21ec2f7b331c7a6b02c
jackson8en <jackson.8en@gmail.com>
2016-11-28 15:36:27 +0000

    SnapPoint Renderer & Colliders, More Shapes.
    Added the disable of used snappoints renderer and collider. Created a
    rectangle object. Nested all ship components into the ship-root. Added a
    rigid body to ship-root for physics later on.

--- Project Space Cube/Assets/Scripts/MouseManager.cs
+++ Project Space Cube/Assets/Scripts/MouseManager.cs
@@ -26,13 +26,29 @@ public class MouseManager : MonoBehaviour {
 				int maskHitObject = 1 << hitInfo.collider.gameObject.layer;
 				if((maskHitObject & snapPointLayerMask) != 0){
 					// Spawn new object
-					Vector3 spawnLocation = hitInfo.collider.transform.position + hitInfo.normal;
-					Quaternion spawnRotation = Quaternion.FromToRotation (Vector3.forward, hitInfo.normal);
+					Vector3 spawnLocation = hitInfo.collider.transform.position;
+					Quaternion spawnRotation = hitInfo.collider.transform.rotation;
+					Transform spawnParent = hitInfo.collider.transform;
 
-					Instantiate(spawnObject, spawnLocation, spawnRotation);
+					GameObject spawnChild = (GameObject) Instantiate(spawnObject, spawnLocation, spawnRotation);
+					spawnChild.transform.SetParent (spawnParent);
+
+					//Disable renderer on snapPoint when object is attached
+					if(hitInfo.collider.GetComponent<Renderer>() != null){
+						hitInfo.collider.GetComponent<Renderer> ().enabled = false;
+					}
+					if(hitInfo.collider.GetComponent<Collider>() != null){
+						hitInfo.collider.GetComponent<Collider> ().enabled = false;
+					}
 				}
 			}
 		}
 	}
+
+//	void RemovePart(GameObject go){
+//		//Re-enable the snap point go is attached to
+//		go.transform.parent.GetComponent<Renderer>() = true;
+//		go.transform.parent.GetComponent<Collider>() = true;
+//	}
 		
 }
